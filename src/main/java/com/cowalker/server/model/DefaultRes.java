574c2df029f848f63f653535aebb933484812a60
bghgu <ektmf1993@gmail.com>
2018-10-19 22:46:53 +0900

    Initial Commit

---
+++ src/main/java/com/cowalker/server/model/DefaultRes.java
@@ -0,0 +1,7 @@
+package com.cowalker.server.model;
+
+/**
+ * Created by ds on 2018-10-19.
+ */
+public class DefaultRes {
+}
---
+++ src/main/java/com/cowalker/server/utils/ResponseMessage.java
@@ -0,0 +1,18 @@
+package com.cowalker.server.utils;
+
+/**
+ * Created by ds on 2018-10-19.
+ */
+public class ResponseMessage {
+    public static final String LOGIN_SUCCESS = "Login Success";
+    public static final String LOGIN_FAIL = "Login Fail";
+
+    public static final String FIND_USER = "Success Find User";
+    public static final String CREATED_USER = "Success Save User";
+    public static final String NOT_FOUND_USER = "Not Find User";
+    public static final String ALREADY_USER = "Already User";
+    public static final String SUCCESS_DELETE_USER = "Success Delete User";
+
+    public static final String UNAUTHORIZED = "UnAuthorization";
+    public static final String INTERNAL_SERVER_ERROR = "Fail";
+}
